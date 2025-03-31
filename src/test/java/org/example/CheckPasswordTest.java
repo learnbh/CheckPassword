f300e8e31ae59d146b70c12b0fbf1b286e6296fd
learnjavabh <learnmsbh@outlook.de>
2025-03-31 11:22:35 +0200

    init

---
+++ src/test/java/org/example/CheckPasswordTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class CheckPasswordTest {
+
+    @Test
+    void checkLength() {
+    }
+
+    @Test
+    void checkContainsNumbers() {
+    }
+
+    @Test
+    void checkUpperAndLowerLetters() {
+    }
+
+    @Test
+    void checkLowerAndUpperLetters() {
+    }
+
+    @Test
+    void checkIsNotLike() {
+    }
+}
