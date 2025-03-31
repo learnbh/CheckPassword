c990cb9ce8da33bd71a6df36e1bfadb347583cd0
learnjavabh <learnmsbh@outlook.de>
2025-03-31 13:03:06 +0200

    implemented checks for length, number, upper+lower case and invalid passwords including tests

--- src/test/java/org/example/CheckPasswordTest.java
+++ src/test/java/org/example/CheckPasswordTest.java
@@ -1,26 +1,62 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CheckPasswordTest {
 
-    @Test
-    void checkLength() {
+    String passwordEqual;
+    String passwordWrong1;
+    String passwordCorrect;
+
+    @BeforeEach
+    void setUp() {
+        passwordEqual = "Password1";
+        passwordWrong1 = "assword";
+        passwordCorrect = "Wort81Pass";
     }
 
     @Test
-    void checkContainsNumbers() {
+    void length_Value_Wort81Pass_shouldReturnTrue_forExpectedLength8(){
+        assertTrue(new CheckPassword(passwordCorrect).length(8));
+    }
+    @Test
+    void length_Value_assword_shouldReturnFalse_forExpectedLength8() {
+        assertFalse(new CheckPassword(passwordWrong1).length(8));
     }
 
     @Test
-    void checkUpperAndLowerLetters() {
+    void containsNumber_shouldReturnTrue_forWord_Password1() {
+        assertTrue(new CheckPassword(passwordEqual).containsNumber());
     }
 
     @Test
-    void checkLowerAndUpperLetters() {
+    void containsNumber_shouldReturnFalse_forWord_assword() {
+        assertFalse(new CheckPassword(passwordWrong1).containsNumber());
     }
 
     @Test
-    void checkIsNotLike() {
+    void containsUpperAndLowerLetters_ShouldReturnTrue_forWord_Password1() {
+        assertTrue(new CheckPassword(passwordEqual).containsUpperAndLowerLetters());
+    }
+
+    @Test
+    void containsUpperAndLowerLetters_ShouldReturnFalse_forWord_assword() {
+        assertFalse(new CheckPassword(passwordWrong1).containsUpperAndLowerLetters());
+    }
+    @Test
+    void containsUpperAndLowerLetters_ShouldReturnFalse_forWord_PASSWORD1() {
+        assertFalse(new CheckPassword("PASSWORD1").containsUpperAndLowerLetters());
+    }
+
+    @Test
+    void isNotLike_ShouldReturnTrue_forWord_Password1() {
+        assertTrue(new CheckPassword(passwordWrong1).isNotLike());
+    }
+    @Test
+    void isNotLike_ShouldReturnFalse_forWord_Password1() {
+        assertFalse(new CheckPassword(passwordEqual).isNotLike());
     }
 }
